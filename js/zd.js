c600956313460a5b239c71d24f44cc9c4d8d7127
Garczarek <adamg.98@o2.pl>
2017-09-17 20:39:43 +0200

    Z.D na 18.09

--- js/zd.js
+++ js/zd.js
@@ -1,8 +1,33 @@
-var zad = [];
+//Z.D Napisać skrypt który oblicza obwód, pole oraz objętość stożka. Dane user podaje z klawiatury, wykorzystaj obiekt Math. Użytkownik wybiera za pomocą pola radio co chce obliczyć. Dane wprowadza w formularzu. Wynik wyświetl w bloku.
+
+
 function funkcja(form) {
-zad[0] = form.login.value;
+var h = form.h.value;
+var r = form.r.value;
+var l = form.l.value;
+if (document.getElementById("obwod").checked) {
+    var obw;
+    obw = 2*Math.PI*r;
+    obw = Math.round(obw);
+    document.getElementById("wynik").innerHTML = "Obwód postawy=" + obw;
+} else {
+    if (document.getElementById("pole").checked) {
+    var pole;
+    pole = Math.PI*r*(r+l);
+    pole = Math.round(pole);
+    document.getElementById("wynik").innerHTML = "Pole= " + pole;
+} else {
+    if (document.getElementById("objetosc").checked) {
+    var ob;
+    ob = 1/3*Math.PI*r*r*h;
+    ob = Math.round(ob);
+    document.getElementById("wynik").innerHTML = "Objętość= " + ob;
+}
+}
+}
+/*zad[0] = form.login.value;
 zad[1] = form.haslo.value;
 zad[2] = form.kolor.value;
 document.getElementById("wynik").innerHTML = "login: " + zad[0] + "<br>Hasło: " + zad[1];
-document.getElementById("wynik").style.color = zad[2];
+document.getElementById("wynik").style.color = zad[2];*/
 }
