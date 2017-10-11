895e8af2a247e0a310303664380cdf56c8cddb68
Garczarek <adamg.98@o2.pl>
2017-10-11 09:04:12 +0200

    gds

---
+++ jquery/jquery.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function(){
+    $("#tekst").click(function(){
+        //$("#lorem").toggle();
+        //$("#lorem").toggle("slow");
+        //$("#lorem").toggle("normal");
+        //$("#lorem").toggle("fast");
+        $("#lorem").toggle(100);
+        $("div").css("background","yellow");
+        $(".tekst").css("background","teal");
+        $("#id1").css("background","grey");
+        $("#id2").css("color","orange");
+        $("#id1").css({"background":"white","color":"red"});
+        $("p").css("font-family","arial","verdana");
+        $("p").css({"color":"red","font-size":"120%"});
+    })
+    $("#zm").click(function(){
+       $("p").css("color","lime")
+    });
+});
+
